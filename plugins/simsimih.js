d6eb75bc0a2f96209c8cb9d1a34b21cd7574305b
imarjunarz <85754686+imarjunarz@users.noreply.github.com>
2021-06-12 16:57:04 +0530

    Update simsimih.js

--- plugins/simsimih.js
+++ plugins/simsimih.js
@@ -14,7 +14,7 @@ const BOT = ":"
 const NOT_FOUNDAC = "*Invaild Request*"
 const NEED_LOCATIONA = "*Invaild Request*"
 
-Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: BOT_DESC}, async (message, match) => {
+Asena.addCommand({pattern: 'dee ?(.*)', fromMe: false, desc: BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
